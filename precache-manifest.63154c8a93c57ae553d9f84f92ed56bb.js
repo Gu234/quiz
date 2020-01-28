1e0f61a8fb8432e0ea89724d33789d96f79890c3
Gu234 <guciofilipek@gmail.com>
2020-01-28 18:29:56 +0100

    Updates

---
+++ precache-manifest.63154c8a93c57ae553d9f84f92ed56bb.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5b4048a3667a7c9caa27c05a81953df5",
+    "url": "/quiz/index.html"
+  },
+  {
+    "revision": "90bcee75b4a76e57d6e2",
+    "url": "/quiz/static/css/main.495a1377.chunk.css"
+  },
+  {
+    "revision": "c4e0ecc1ab45e1e0756e",
+    "url": "/quiz/static/js/2.04a1fd99.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/quiz/static/js/2.04a1fd99.chunk.js.LICENSE"
+  },
+  {
+    "revision": "90bcee75b4a76e57d6e2",
+    "url": "/quiz/static/js/main.ffd794dd.chunk.js"
+  },
+  {
+    "revision": "f26692415da7c5bea96a",
+    "url": "/quiz/static/js/runtime-main.f4e90a74.js"
+  }
+]);
